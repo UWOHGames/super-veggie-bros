928062b5ff2b6a85cf54b25aa383f7fc7dbdc154
Charlie Duong <charlieduong94@gmail.com>
2018-02-01 19:42:15 -0500

    Add precommit linting

--- src/globals.js
+++ src/globals.js
@@ -3,8 +3,13 @@
  *
  * This needs to be done to properly use Phaser
  */
-window.PIXI = require('phaser-ce/build/custom/pixi')
-window.p2 = require('phaser-ce/build/custom/p2')
-window.Phaser = require('phaser-ce/build/custom/phaser-split')
+const PIXI = window.PIXI =
+  require('phaser-ce/build/custom/pixi')
+
+const p2 = window.p2 =
+  require('phaser-ce/build/custom/p2')
+
+const Phaser = window.Phaser =
+  require('phaser-ce/build/custom/phaser-split')
 
 module.exports = { PIXI, p2, Phaser }
